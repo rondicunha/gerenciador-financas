3ea951f6498fdcc3409a57ef87aa5319f0afc0d6
Rondinelle Cunha <rondinelle.cunha.101@ufrn.edu.br>
2023-07-28 21:50:41 -0300

    Inserindo api publicada

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gerenciador-financas-back.vercel.app",
 });
 
 export default api;
